060770eaaf8fda53260a98ddfafe7b050fb63957
Gonzalo Chumillas <gchumillas@email.com>
2015-06-28 20:33:57 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sys
+A library to interact with the OS. This library is part of the SoloProyectos PHP API.
---
+++ src/sys/exception/SysException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of SoloProyectos common library.
+ *
+ * @author  Gonzalo Chumillas <gchumillas@email.com>
+ * @license https://github.com/soloproyectos-php/sys/blob/master/LICENSE The MIT License (MIT)
+ * @link    https://github.com/soloproyectos-php/sys
+ */
+namespace soloproyectos\sys\exception;
+use \Exception;
+
+/**
+ * Class SysException.
+ *
+ * @package Sys\Exception
+ * @author  Gonzalo Chumillas <gchumillas@email.com>
+ * @license https://github.com/soloproyectos-php/sys/blob/master/LICENSE The MIT License (MIT)
+ * @link    https://github.com/soloproyectos-php/sys
+ */
+class SysException extends Exception
+{
+
+}
